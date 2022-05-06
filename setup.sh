ff6d080241fb08f851657eac7623124ceefb15a5
Martin Klingenberg <martin.klingenberg@alv.no>
2022-05-06 13:31:48 +0200

    Updated setup script and set fish as default shell

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
-sudo dnf install tmux
-sudo dnf install tmux-powerline
+#!/bin/env bash
+sudo dnf install tmux-powerline tmux ruby fish
 cp ./tmux.conf ~/.tmux.conf
 
 mkdir -p ~/.tmux
 mkdir -p ~/.config/tmuxinator
 
+
+
 cp ./tmuxinator-configs/* ~/.config/tmuxinator
+gem install tmuxinator
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
