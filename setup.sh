5dbd39243128e41bf405fb1c69623fc22b8c0cae
klyngen <martin@endre.app>
2026-05-27 23:22:45 +0200

    using new config folder

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/env bash
 sudo dnf install tmux-powerline tmux ruby fish
-cp ./tmux.conf ~/.tmux.conf
 
-mkdir -p ~/.tmux
+mkdir -p ~/.config/tmux
 mkdir -p ~/.config/tmuxinator
 
+cp ./tmux.conf ~/.config/tmux/tmux.conf
 
 
 cp ./tmuxinator-configs/* ~/.config/tmuxinator
 gem install tmuxinator
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
